66a9158d4f2610c9d69c37e6dcdd2c5d47b76a75
XDRONIN <akashprakashmattathil@gmail.com>
2024-05-24 14:14:51 +0530

    object data in the card

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ form.addEventListener("submit", (e) => {
   const fd = new FormData(form);
   const dataobj = Object.fromEntries(fd);
   console.log(dataobj);
-  createCard();
+  createCard(dataobj);
 });
 const infoDiv = document.querySelector(".bookInfo");
-function createCard() {
+function createCard(dataobj) {
   const cardDiv = document.createElement("div");
   cardDiv.classList.add("card");
+
+  let valuesString = "";
+  for (var key in dataobj) {
+    if (dataobj.hasOwnProperty(key)) {
+      valuesString += key + dataobj[key] + "<br>";
+    }
+  }
+
+  cardDiv.innerHTML = valuesString;
   infoDiv.appendChild(cardDiv);
   document.appendChild(infoDiv);
 }
