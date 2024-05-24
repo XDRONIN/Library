457e2d5be05a19abb99f7db60dbe42bb2ef82610
XDRONIN <akashprakashmattathil@gmail.com>
2024-05-24 12:27:32 +0530

    popup functionality of add button and the submit functionality of submit button

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const addButton = document.querySelector(".addButton");
-addButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+  document.querySelector(".popup").classList.add("active");
+});
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fd = new FormData(form);
+  const dataobj = Object.fromEntries(fd);
+  console.log(dataobj);
+});
